cd5f699f4645a0101a25fd6ec0db82d788d1626e
kamal Prajapat <kamalprajapat7117@gmail.com>
2025-07-12 22:44:49 +0530

    Start

---
+++ PractiseCode/Palindrom.java
@@ -0,0 +1,26 @@
+// 9. Palindrome Number
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+// Example 1:
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+
+import java.util.Arrays;
+class Palindrom {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false; // Negative numbers are not palindromes
+        int original = x, reversed = 0;
+        while (x > 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+}
