30489a187e6bc2bacd7cfc1595994ab0a489dd4a
kamal Prajapat <kamalprajapat7117@gmail.com>
2025-07-12 23:42:17 +0530

    ArmStrongNumber Add

---
+++ PractiseCode/ArmStrongNumber.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+class ArmStrongNumber {
+    static boolean armstrongNumber(int n) {
+        // code here
+        int len = String.valueOf(n).length();
+        int temp = n;
+        int res = 0;
+        int x = 0;
+        while(temp !=0){
+            x = temp%10;
+            temp = temp/10;
+            res += Math.pow(x,len);
+        }
+        
+        return res == n;
+    }
+}
